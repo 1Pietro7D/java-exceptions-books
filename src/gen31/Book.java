94ece833012e79474d0acd723e9fcd48a31c7fb8
PietroDanieli <1pie7d@gmail.com>
2023-02-01 02:22:31 +0100

    add some Exception

--- src/gen31/Book.java
+++ src/gen31/Book.java
@@ -6,11 +6,30 @@ class Book {
 	private String author;
 	private String publisher;
 
-	public Book(String title, int numPages, String author, String publisher) throws Exception {
-		setTitle(title);
-		setNumPages(numPages);
-		setAuthor(author);
-		setPublisher(publisher);
+	public Book(String title, String numPages, String author, String publisher) throws Exception {
+		  if (title.isEmpty()) {
+		      throw new Exception("Il titolo non può essere vuoto");
+		    }
+		  try {
+			  if (Integer.parseInt(numPages) <= 0) {
+			      throw new Exception("Il numero di pagine deve essere maggiore di 0");
+			    }
+			} catch (NumberFormatException e) {
+			  System.out.println("Input non valido, inserire un numero intero");
+			}
+		    if (Integer.parseInt(numPages) <= 0) {
+		      throw new Exception("Il numero di pagine deve essere maggiore di 0");
+		    }
+		    if (author.isEmpty()) {
+		      throw new Exception("L'autore non può essere vuoto");
+		    }
+		    if (publisher.isEmpty()) {
+		      throw new Exception("L'editore non può essere vuoto");
+		    }
+		    this.title = title;
+		    this.numPages = Integer.parseInt(numPages);
+		    this.author = author;
+		    this.publisher = publisher;
 	}
 
 	public void setTitle(String title) throws Exception {
@@ -23,11 +42,13 @@ class Book {
 	public String getTitle() {
 		return title;
 	}
-
+	public void setNumPages(Error numPages) throws Exception {
+			throw new Exception("Il numero di pagine deve essere un numero maggiore di 0");
+	}
 	public void setNumPages(int numPages) throws Exception {
 		if (numPages <= 0) {
 			throw new Exception("Il numero di pagine deve essere maggiore di 0");
-		}
+		} 
 		this.numPages = numPages;
 	}
 
@@ -55,6 +76,15 @@ class Book {
 
 	public String getPublisher() {
 		return publisher;
+	}
+	
+	@Override
+	public String toString() {
+	  return "Informazioni sul prodotto: " +
+	         "Title = " + this.getTitle() +
+	         ", Author = " + this.getAuthor() +
+	         ", n. pages = " + this.getNumPages() +
+	         ", Publisher = " + this.getPublisher();
 	}
 
 }
--- src/gen31/Main.java
+++ src/gen31/Main.java
@@ -5,33 +5,70 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Inserire il numero di libri: ");
-		int n = Integer.parseInt(sc.nextLine());
-		Book[] books = new Book[n];
-		for (int i = 0; i < n; i++) {
-			System.out.println("Inserire i dati del libro " + (i + 1));
-			System.out.print("Titolo: ");
-			String title = sc.nextLine();
 
-			System.out.print("Numero di pagine: ");
-			int numPages = sc.nextInt();
-			sc.nextLine();
-
-			System.out.print("Autore: ");
-			String author = sc.nextLine();
-
-			System.out.print("Editore: ");
-			String publisher = sc.nextLine();
-
+		do {
 			try {
-				books[i] = new Book(title, numPages, author, publisher);
-			} catch (Exception e) {
-				System.out.println("Errore: " + e.getMessage());
-				i--;
-			}
-		}
+				System.out.print("Inserire il numero di libri: ");
+				int n = Integer.parseInt(sc.nextLine());
+				Book[] books = new Book[n];
 
-		sc.close();
-		System.out.println("libro 1 title -> " + books[0].getTitle());
+				for (int i = 0; i < n; i++) {
+					try {
+						System.out.println("Inserire i dati del libro " + (i + 1));
+						System.out.print("Titolo: ");
+						String title = sc.nextLine();
+
+						System.out.print("Numero di pagine: ");
+						String numPages = sc.nextLine(); // Errore: For input string: "qwe"???
+
+						System.out.print("Autore: ");
+						String author = sc.nextLine();
+
+						System.out.print("Editore: ");
+						String publisher = sc.nextLine();
+
+						books[i] = new Book(title, numPages, author, publisher);
+					} catch (Exception e) {
+						System.out.println("Errore: " + e.getMessage());
+						// e.printStackTrace();
+						i--;
+					}
+				}
+
+				System.out.println("your's list books:");
+				for (Object book : books) {
+					System.out.println(book.toString());
+				}
+
+			} catch (NumberFormatException e) {
+				System.out.println("Errore: Il numero di libri deve essere un numero intero");
+			} catch (NegativeArraySizeException e) {
+				System.out.println("Errore: Il numero di libri deve essere un numero intero POSITIVO");
+			} finally {
+				sc.close();
+			}
+			;
+			;
+		} while (true);
+
 	}
 }
+
+
+/*Possibile soluzione per visualizzare tutti gli array
+List<String> errorList = new ArrayList<>();
+for (int i = 0; i < n; i++) {
+  try {
+    ...
+  } catch (Exception e) {
+    errorList.add("Errore nell'input del libro " + (i + 1) + ": " + e.getMessage());
+    i--;
+  }
+}
+
+if (!errorList.isEmpty()) {
+  System.out.println("Errori:");
+  for (String error : errorList) {
+    System.out.println(error);
+  }
+}*/
